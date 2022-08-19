d43a9eb4c769a358efcac2ac32bbd9b7a7dfab6d
naoyuki42 <naoyuki.7042@outlook.jp>
2022-08-19 18:41:28 +0900

    add: security group module

---
+++ modules/security/security_group/main.tf
@@ -0,0 +1,29 @@
+# セキュリティグループ
+resource "aws_security_group" "sg" {
+  name   = var.name
+  vpc_id = var.vpc_id
+
+  tags = merge({
+    Name = "${var.env}-security-group-for-${var.name}"
+  })
+}
+
+# インバウンドルール
+resource "aws_security_group_rule" "ingress" {
+  type              = "ingress"
+  from_port         = var.port
+  to_port           = var.port
+  protocol          = "tcp"
+  cidr_blocks       = var.cidr_blocks
+  security_group_id = aws_security_group.sg.id
+}
+
+# アウトバウンドルール
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = "0"
+  to_port           = "0"
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg.id
+}
